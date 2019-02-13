bdf6ec84f4cd9cccac6a55f0f7bfb92ffe636386
cwinters8 <clwinters8@gmail.com>
2019-02-12 20:50:11 -0800

    successfully rendering gifs

--- src/Components/GifList.js
+++ src/Components/GifList.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Gif from './Gif';
 
 const GifList = props => { 
+
+  const results = props.data;
+  const gifs = results.map(gif => {
+    return <Gif img={gif.images.fixed_height.url} key={gif.id} />
+  })
   
   return(
     <ul className="gif-list">
-      {/* <Gif /> */}
+      {gifs}
     </ul> 
   );
 }
