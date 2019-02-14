9a3a56f3c621f2417e58f2b3a2e1fe3447603a70
cwinters8 <clwinters8@gmail.com>
2019-02-13 21:04:54 -0800

    update to https

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
   }
 
   performSearch = (query = 'cats') => {
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=URrM0D5kxVE1yOepv7xAATKzoxwCkoJ5`)
+    axios.get(`https://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=URrM0D5kxVE1yOepv7xAATKzoxwCkoJ5`)
     .then(response => {
       this.setState({
         gifs: response.data.data,
